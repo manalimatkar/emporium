61d1f80326e21f123050eeaf377f1945e74691fc
manalimatkar <manali.matkar@gmail.com>
2017-06-21 14:07:49 -0500

    design update

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@ var myCart = [];
 $(document).ready(function() {
     // Get data from JSON
 
+   $('#itemAdded').hide();
     var getSpaceShipList = function() {
             var apiUrl = "https://demo7475333.mockable.io/spaceships";
             $.ajax({ url: apiUrl, method: 'GET' }).done(function(response) {
@@ -36,18 +37,31 @@ $(document).ready(function() {
 
             for (var i = 0; i < spaceships.length; i++) {
                 // Container div
-                var containerDiv = $("<div class='col-md-3 col-sm-6 img-portfolio' data-toggle='modal' data-target='#detailView'>");
-                containerDiv.attr('data-index', i)
-                    // Grid Image Icon
-                var imgTag = $("<img class='img-responsive img-hover img-center'>");
-                imgTag.attr("src", spaceships[i].imgIconSrc);
-                // Append Img to container div
-                containerDiv.append(imgTag);
-                // Ship Name
-                containerDiv.append("<p class='text-center'><strong>" + spaceships[i].name + "</strong></p>");
-                // Ship Class 
-                containerDiv.append("<h4>" + spaceships[i].class + "</h4>");
+                var containerDiv = $("<div class='col-md-4 col-sm-6 img-portfolio' data-toggle='modal' data-target='#detailView'>");
+                containerDiv.attr('data-index', i);
+                // Create Panel
+                var containerPanel = $("<div class='panel panel-primary'>");
+                // panel heading
+                var panelHeading = $("<div class='panel-heading'>");
+                panelHeading.text(spaceships[i].name);
+                // add panel heading to panel
+                containerPanel.append(panelHeading);
+                // panel body
+                var panelBody = $("<div class='panel-body'>");
 
+                // Grid Image Icon
+                var imgTag = $("<img class='img-responsive img-hover img-center' style='width:100%'' alt='Image'>");
+                imgTag.attr("src", spaceships[i].imgIconSrc);
+                // Append Img to panel body
+                panelBody.append(imgTag);
+
+                var panelFooter = $("<div class='panel-footer'>");
+                panelFooter.text(spaceships[i].class);
+
+                containerPanel.append(panelHeading);
+                containerPanel.append(panelBody);
+                containerPanel.append(panelFooter);
+                containerDiv.append(containerPanel);
                 $("#spaceshipsDisplayGrid").append(containerDiv);
             }
 
@@ -57,23 +71,32 @@ $(document).ready(function() {
     $(document).on('click', '.img-portfolio', function() {
         // Get user id from userid data attr
         var selectedShip = $(this).attr('data-index');
+        // $(this).addClass("animated slideOutDown hidden");
         console.log(selectedShip);
         // Get current user data and populate modal 
         populateModal(selectedShip);
     });
-    $(document).on("click", "#addToCart", function(){
-    	var itemPurchased = $("#addToCart").attr("data-item");
-    	
-    	myCart.push(spaceships[itemPurchased]);
-    	console.log(myCart);
-    	$("#detailView").modal('toggle');
-    	$("#shopingCart").text("Items Purchased : " + myCart.length);
+    $(document).on("click", "#addToCart", function() {
 
+        var itemPurchased = $("#addToCart").attr("data-item");
+        myCart.push(spaceships[itemPurchased]);
+        console.log(myCart);
+        $("#detailView").modal('toggle');
+        $("#shopingCart").text(myCart.length);
+        $(window).scrollTop($('#shopingCart'));
+        
+         $("#itemAdded").show();       
+
+        // $("#shopingCart").focus();
+
+    });
+    $('.close').click(function() {
+        $('#itemAdded').hide();
     });
 
     // Bind clicked selectedShip data to modal
     var populateModal = function(selectedShip) {
-    	$("#addToCart").attr("data-item", selectedShip);
+        $("#addToCart").attr("data-item", selectedShip);
         // Name
         var shipClass = spaceships[selectedShip].class;
         var shipImgSrc = spaceships[selectedShip].imgProfile;
@@ -94,5 +117,5 @@ $(document).ready(function() {
         }
         // Show Modal on page
         $('#detailView').modal('show');
-    }  
+    }
 }); //Document Ready Ends
